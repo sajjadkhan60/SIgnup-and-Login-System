2c415b3c7518cd2c6d605c0554b2205344947e58
Sajjad Ahmad <goldennote60@gmail.com>
2022-12-02 01:22:21 +0500

    dashboard is under implmentation

--- src/pages/dashboard_page/dashboard.page.js
+++ src/pages/dashboard_page/dashboard.page.js
@@ -1,32 +1,67 @@
-import React, { useEffect } from "react";
-import {  useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./dashboard.page.css";
 
 function Dashboard() {
+  const [posts, setPosts] = useState("Loading...");
+  const [userName, setUserName] = useState("____");
   let navigate = useNavigate();
-  useEffect(()=>{
+  useEffect(() => {
     if (localStorage.getItem("userEmail") === null) {
       navigate(`/`);
-    }else{
-      const userEmail  = window.localStorage.getItem("userEmail");
-      console.log(userEmail);
+    } else {
+      const userEmail = window.localStorage.getItem("userEmail");
+      // Fetch User details and posts
+      fetch(
+        "http://localhost/firegram/load-posts.php?email=" + userEmail + "",
+        {
+          method: "GET",
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setUserName(data.username);
+          if (data.posts == "none") {
+            setPosts(false);
+          } else {
+            setPosts(data.posts);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     }
-    
-  },[])
-  function logout(){
+  }, []);
+  function logout() {
     window.localStorage.removeItem("userEmail");
     navigate(`/`);
   }
   return (
     <div>
-      <div className="container">
-        <h1>Welcome Back!</h1>
-        <p>This is your dashboard</p>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <h1>{userName}!</h1>
+        </div>
         <div className="signuplink">
           <span className="link" onClick={logout}>
-              Logout
+            Logout
           </span>
         </div>
+      </div>
+      <div className="container">
+        <h3>My Posts</h3>
+        {posts ? (
+          <showPosts posts={posts} />
+        ) : (
+          <div className="no-posts">No posts to show!</div>
+        )}
       </div>
     </div>
   );
